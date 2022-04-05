b7611f3c172f4f627c00c3ed71004db035ff3ac2
jamesarrojo.01 <jamesarrojo.01@gmail.com>
2022-04-05 10:48:34 +0800

    fixed for B_1 linear interpolation if fc' is between 28 and 56 MPa

--- SRB/index.js
+++ SRB/index.js
@@ -6,6 +6,9 @@ function srbAnalysis(As, b, d, fy, fcPrime) {
         betaOne = 0.85
     } else if (fcPrime >= 55) {
         betaOne = 0.65
+    } else {
+        let y = 0.85 + ((fcPrime - 28) / (56 - 28)) * (0.65 - 0.85)
+        betaOne = y.toFixed(2)
     }
     console.log(`B_1 is ${betaOne}`)
     let rho = As / (b*d)
@@ -36,7 +39,7 @@ function srbAnalysis(As, b, d, fy, fcPrime) {
         let fs = 600 (d - c)/c`)
         const A = 1
         const B = (As * 600)/(0.85 * fcPrime * betaOne)
-        const C = (-1)(As * 600 * d)/(0.85 * fcPrime * betaOne)
+        const C = -(As * 600 * d)/(0.85 * fcPrime * betaOne)
         c = (-B + Math.sqrt((B**2) - 4*A*C))/(2 * A)
         a = betaOne * c
         Mn = ((As * fy * (d - (a/2)))/1000**2).toFixed(2)
@@ -60,7 +63,7 @@ function srbAnalysis(As, b, d, fy, fcPrime) {
         // let input = prompt(`Please enter the what you got in your calculator`)
 
         let y = 0.90 + ((strain - 0.005) / (0.002 - 0.005)) * (0.65 - 0.90)
-        phi = Number(y).toFixed(2)
+        phi = y.toFixed(2)
         console.log(`Φ is ${phi}`)
     }
 
